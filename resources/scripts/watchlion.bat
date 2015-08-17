a4c810d0abcd81722a75b1fcb46b6a15fc589d37
seiza <jacques@couvreur.me>
2015-08-17 13:31:51 +0100

    Bugfix launch script for windows.

--- resources/scripts/watchlion.bat
+++ resources/scripts/watchlion.bat
@@ -1,1 +1,7 @@
-java -jar watchlion-0.0.1-SNAPSHOT-executable.jar -reference watchlion.reference.json
+echo off
+echo ### 
+echo ### WARNING: 'mvn clean install' needed to avoid this script (Watchlion .jar would be missing elsewhere)
+echo ### 
+cd ../..
+java -jar target/watchlion-0.0.1-SNAPSHOT-executable.jar -reference src/main/resources/watchlion.reference.json
+pause
